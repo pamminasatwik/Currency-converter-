4bb9b08acf0cd62c1ce51d8dacbcb584a0e714f5
Satwik <20h51a0543@cmrcet.ac.in>
2023-02-24 08:51:27 +0530

    cool

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const app=express()
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 6000;
 
 // app.use(express.json())
 app.get('/currency', function(req, res) {
